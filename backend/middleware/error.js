054e77d34fe5c284f4254d92a2a375d8d5e67623
Beepul <79615064+Beepul@users.noreply.github.com>
2024-01-04 18:52:57 +0545

    morgan fix

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,10 +1,10 @@
-const { errorLogger } = require("./morgan");
+// const { errorLogger } = require("./morgan");
 
 module.exports = (err,req,res,next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal server Error"
 
-    errorLogger(err.message)
+    // errorLogger(err.message)
 
     res.status(err.statusCode).json({
         success: false,
--- backend/middleware/morgan.js
+++ backend/middleware/morgan.js
@@ -2,14 +2,15 @@ const morgan = require("morgan");
 const logger = require("./logger");
 
 
-const errorLogger = (message) => {
-  morgan.token('error', (req,res) => {
-    return message
-  })
-}
+// const errorLogger = (message) => {
+//   morgan.token('error', (req,res) => {
+//     return message
+//   })
+// }
 
 const successResponseFormat = `:method :url :status - :response-time ms`;
-const errorResponseFormat = `:method :url :status - :response-time ms - message: :error`;
+const errorResponseFormat = `:method :url :status - :response-time ms`;
+// const errorResponseFormat = `:method :url :status - :response-time ms - message: :error`;
 
 const successHandler = morgan(successResponseFormat, {
   skip: (req, res) => res.statusCode >= 400,
